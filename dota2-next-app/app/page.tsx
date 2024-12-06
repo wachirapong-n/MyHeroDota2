0495801e0fb521046a706bccfb0edee2713479d8
wachirapong-n <wachirapong_n@cmu.ac.th>
2024-12-06 22:35:03 +0700

    add filter func

--- dota2-next-app/app/page.tsx
+++ dota2-next-app/app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { useEffect, useState } from "react";
+import ".//styles/favIcon.css";
 
 export default function Home() {
-  const [heroData, setHeroData] = useState([]);
+  const [allHero, setAllHero] = useState([]);
   const [filteredHero, setFilteredHero] = useState([]);
   const [searchedHero, setSearchedHero] = useState("");
   const [sorted, setSorted] = useState("asc");
-  const [filterArr, setfilterArr] = useState([]);
+  const [filterArr, setFilterArr] = useState([]);
   const path = 'https://cdn.cloudflare.steamstatic.com';
 
 
@@ -14,10 +15,17 @@ export default function Home() {
     const fetchData = async () => {
       try {
         const res = await fetch('https://api.opendota.com/api/heroStats');
-        const heroData = await res.json();
-        var sortedHero = heroData.sort((a, b) =>
+        const allHero = await res.json();
+
+        allHero.forEach(hero => {
+          var temp = [...hero.roles, hero.primary_attr, hero.attack_type];
+          hero.roles = temp;
+        });
+
+        console.log(allHero)
+        var sortedHero = allHero.sort((a, b) =>
           a.localized_name.localeCompare(b.localized_name));
-        setHeroData(sortedHero);
+        setAllHero(sortedHero);
         setFilteredHero(sortedHero);
       } catch (error) {
         console.log("Can not fetch data:", error);
@@ -29,7 +37,7 @@ export default function Home() {
 
   const handleSearch = (e) => {
     var heroName = e.target.value;
-    var heroes = heroData.filter((hero) =>
+    var heroes = allHero.filter((hero) =>
       hero.localized_name.toLowerCase().includes(heroName.toLowerCase()))
     setFilteredHero(heroes);
     setSearchedHero(heroName);
@@ -38,25 +46,80 @@ export default function Home() {
   const handleSort = () => {
     if (sorted === "asc") {
       setSorted("desc");
-      var sortedHero = heroData.sort((a, b) =>
+      var sortedHero = allHero.sort((a, b) =>
         b.localized_name.localeCompare(a.localized_name));
       setFilteredHero(sortedHero);
     }
     else {
       setSorted("asc");
-      var sortedHero = heroData.sort((a, b) =>
+      var sortedHero = allHero.sort((a, b) =>
         a.localized_name.localeCompare(b.localized_name));
       setFilteredHero(sortedHero);
     }
   }
 
-  const handleCheck = (e) => {
-    var filteredVal = e.target.value;
-    setfilterArr(filterArr + filteredVal)
-    var heroes = filterArr.every(role => hero.roles.includes(role));
-    console.log(heroes)
+  const handleCheckbox = (e) => {
+    var arr = [];
+    var type = e.target.value;
+    if (e.target.checked) {
+      if (type === "melee" || type === "ranged") {
+        var boxes = document.getElementById("atkType").querySelectorAll("input[type=checkbox]");
+        boxes.forEach(box => {
+          box.checked = false;
+        });
+
+        e.target.checked = true;
+        arr = removeType(filterArr, ["melee", "ranged"])
+        arr = arr.concat(type)
+
+      } else if (type === "agi" || type === "str" || type === "int" || type === "all") {
+        var boxes = document.getElementById("attr").querySelectorAll("input[type=checkbox]");
+        boxes.forEach(box => {
+          box.checked = false;
+        });
+
+        e.target.checked = true;
+        arr = removeType(filterArr, ["agi", "str", "int", "all"])
+        arr = arr.concat(type)
+
+      } else {
+        arr = filterArr.concat(e.target.value)
+
+      }
+
+    }
+    else {
+      arr = filterArr.filter((val) => val !== e.target.value);
+
+    }
+    setFilterArr(arr);
+
+    var filtered = allHero.filter((hero) => filterByCheckedbox(hero, arr));
+    var filteredBySearch = filtered.filter((hero) =>
+      hero.localized_name.toLowerCase().includes(searchedHero.toLowerCase()))
+
+    setFilteredHero(filteredBySearch);
 
   }
+
+  function removeType(arr, target) {
+    target.forEach(val => {
+      var indx = arr.indexOf(val);
+      if (indx != -1) {
+        arr.splice(indx, 1);
+      }
+    });
+    return arr;
+  }
+
+  function filterByCheckedbox(hero, arr) {
+    var roles = hero.roles;
+    roles = roles.map((tag) => tag.toLowerCase());
+    const isSubset = (a, b) => b.every((val) => a.includes(val));
+    return isSubset(roles, arr);
+  }
+
+
 
   return (
     <div>
@@ -76,62 +139,66 @@ export default function Home() {
           <input
             type="checkbox"
             value="carry"
+            onChange={handleCheckbox}
           />
           <label>Carry</label>
           <br />
           <input
             type="checkbox"
             value="support"
+            onChange={handleCheckbox}
           />
           <label>Support</label>
           <br />
         </div>
 
-        <div>
+        <div id="atkType">
           <span>Attack Type</span>
           <br />
           <input
             type="checkbox"
             value="melee"
-
-            onChange={handleCheck}
+            onChange={handleCheckbox}
           />
           <label>Melee</label>
           <br />
           <input
             type="checkbox"
             value="ranged"
-
-            onChange={handleCheck}
+            onChange={handleCheckbox}
           />
           <label>Ranged</label>
           <br />
         </div>
 
-        <div>
+        <div id="attr">
           <span>Attributes</span>
           <br />
           <input
             type="checkbox"
             value="str"
+            onChange={handleCheckbox}
           />
           <label>Strength</label>
           <br />
           <input
             type="checkbox"
-            value="agility"
+            value="agi"
+            onChange={handleCheckbox}
           />
           <label>Agility</label>
           <br />
           <input
             type="checkbox"
             value="int"
+            onChange={handleCheckbox}
           />
           <label>Intelligence</label>
           <br />
           <input
             type="checkbox"
             value="all"
+            onChange={handleCheckbox}
           />
           <label>Universal</label>
           <br />
@@ -143,36 +210,42 @@ export default function Home() {
           <input
             type="checkbox"
             value="disabler"
+            onChange={handleCheckbox}
           />
           <label>Disabler</label>
           <br />
           <input
             type="checkbox"
             value="durable"
+            onChange={handleCheckbox}
           />
           <label>Durable</label>
           <br />
           <input
             type="checkbox"
             value="escape"
+            onChange={handleCheckbox}
           />
           <label>Escape</label>
           <br />
           <input
             type="checkbox"
             value="initiator"
+            onChange={handleCheckbox}
           />
           <label>Initiator</label>
           <br />
           <input
             type="checkbox"
             value="nuker"
+            onChange={handleCheckbox}
           />
           <label>Nuker</label>
           <br />
           <input
             type="checkbox"
             value="pusher"
+            onChange={handleCheckbox}
           />
           <label>Pusher</label>
           <br />
@@ -206,7 +279,12 @@ export default function Home() {
             {filteredHero.map((hero) => (
               <tr key={hero.id}>
                 <th>
-                  <img src={path + hero.img}></img>
+                  <div className="container">
+                    <a href={`/heroes/${hero.id}`}><img src={path + hero.img}></img></a>
+
+                    <button className="btn">&#9733;</button>
+                  </div>
+
                 </th>
                 <th>
                   <a href={`/heroes/${hero.id}`}>{hero.localized_name}</a>
